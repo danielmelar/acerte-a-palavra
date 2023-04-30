cc95d9714c0a8bdf55e8d4b80106dc4d51ccdb67
daniel <danielmelo.aguilar@gmail.com>
2023-04-30 19:05:36 -0300

    P2|Sorteia indice aleatorio

--- codigoGo/main.go
+++ codigoGo/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main(){
-	fmt.Println("Hello, World!")
+	palavras := []string{"verdadeiro", "criança", "jogo", "caderno", "notebook"}
+	
+	indiceAleatorio := sorteiaIndiceAleatorio(palavras)
+
+	fmt.Println(indiceAleatorio)
+	fmt.Println(palavras[indiceAleatorio])
 }
+
+func sorteiaIndiceAleatorio(array []string) int {
+	indiceSorteado := rand.Intn(len(array))
+
+	return indiceSorteado
+}
+
+
+
+
+
+
+
+
+
+
